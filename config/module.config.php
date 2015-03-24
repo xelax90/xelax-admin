1c2cb72d27be85735658d11d0a210da0755f07cd
Aleksandr <schurix@gmx.de>
2015-03-24 03:31:25 +0100

    reverted module config test changes

--- config/module.config.php
+++ config/module.config.php
@@ -19,73 +19,6 @@ namespace XelaxAdmin;
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-$xelaxConfig = array(
-	/*
-	 * Configure your list controllers. Routes are generated automatically, but
-	 * you can also create your own routes. Route names must correspond with 
-	 * controller names.
-	 */
-	'list_controller' => array(
-		'menus' => array(
-			'name' => 'Menu', // this will be the route url and is used to generate texts
-			'controller_class' => 'XelaxAdmin\Controller\ListController',
-			// route_base defaults to the config key ('menus' in this case). 
-			'route_base' => 'menus_foo', // only available at top-level options
-			'create_route' => array(
-				'allowed_roles' => array('moderator'),
-			),
-			'edit_route' => array(
-				'allowed_roles' => array('moderator'),
-			),
-			'delete_route' => array(
-				'allowed_roles' => array('administrator'),
-			),
-			'child_options' => array(
-				'item' => array(
-					'name' => 'Item',
-					'controller_class' => 'XelaxAdmin\Controller\ListController',
-					'list_title' => 'Menu Items', 
-					'create_route' => array(
-						'allowed_roles' => array('moderator'),
-					),
-					'edit_route' => array(
-						'allowed_roles' => array('moderator'),
-					),
-					'delete_route' => array(
-						'allowed_roles' => array('administrator'),
-					),
-					// for other options, see XelaxAdmin\Options\ListControllerOptions
-				),
-			),
-		),
-	),
-);
-
-$routerConfig = array(
-	/**
-	 * Configure the routes pointing to the list.
-	 * The route name can be chosen arbitrary, but has to be linked in 
-	 * the controller options as 'route_base' if it differs from the options key
-	 */
-	'menus_foo' => array(
-		'type' => 'XelaxAdmin\Router\ListRoute',
-		'options' => array(
-			// the config key of the options
-			'controller_options_name' => 'menus',
-		),
-	),
-);
-
-$guardConfig = array(
-	/**
-	 * Each route needs a guard, or it cannot be accessed.
-	 * The Access control inside the list controller options is an additional filter.
-	 * You can make the route unrestricted and restrict access to specific actions inside the list controller
-	 */
-	array('route' => 'menus_foo', 'roles' => array('user', 'guest')),
-);
-
-
 return array(
 	'service_manager' => array(
 		'factories' => array(
@@ -123,15 +56,9 @@ return array(
 		),
 	),
 
-    'xelax' => $xelaxConfig,
-	'router' => array(
-		'routes' => $routerConfig,
-	),
-
 	'bjyauthorize' => array(
 		'guards' => array(
 			'XelaxAdmin\Guard\ListController' => null,
-            'BjyAuthorize\Guard\Route' => $guardConfig,
 		),
 		
         'resource_providers' => array(
